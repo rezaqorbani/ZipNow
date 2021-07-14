247e55ad90603c9cce329113fd7de8521b47065c
Reza Qorbani <reza3qorbani@gmail.com>
2021-07-14 22:23:28 +0200

    create findsize and readfile

--- main.c
+++ main.c
@@ -1,22 +1,87 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-bool lz77_compress(const uint8_t *src, size_t src_len, size_t max_dist,
-                   size_t max_len, bool allow_overlap,
-                   bool (*lit_callback)(uint8_t lit, void *aux),
-                   bool (*backref_callback)(size_t dist, size_t len, void *aux),
-                   void *aux);
+#define PERROR_IF(cnd, msg) \
+    do                      \
+    {                       \
+        if (cnd)            \
+        {                   \
+            perror(msg);    \
+            exit(1);        \
+        }                   \
+    } while (0)
 
-int main()
+long int findSize(char *file_name)
 {
-    size_t sizeArray[] = {1, 2, 3, 4, 5};
+    // opening the file in read mode
+    FILE *fp = fopen(file_name, "r");
 
-    for (int i = 0; i < 5; i++)
+    // checking if the file exist or not
+    if (fp == NULL)
     {
-
-        printf("%d\n", sizeArray[i]);
+        printf("File Not Found!\n");
+        return -1;
     }
 
+    fseek(fp, 0L, SEEK_END);
+
+    // calculating the size of the file
+    long int res = ftell(fp);
+
+    // closing the file
+    fclose(fp);
+
+    return res;
+}
+
+static void *xmalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    PERROR_IF(ptr == NULL, "malloc");
+    return ptr;
+}
+
+static void *xrealloc(void *ptr, size_t size)
+{
+    ptr = realloc(ptr, size);
+    PERROR_IF(ptr == NULL, "realloc");
+    return ptr;
+}
+
+static uint8_t *read_file(const char *filename, size_t *file_sz)
+{
+    FILE *f;
+    uint8_t *buf;
+    size_t buf_cap;
+
+    f = fopen(filename, "rb");
+    PERROR_IF(f == NULL, "fopen");
+
+    buf_cap = 4096;
+    buf = xmalloc(buf_cap);
+
+    *file_sz = 0;
+    while (feof(f) == 0)
+    {
+        if (buf_cap - *file_sz == 0)
+        {
+            buf_cap *= 2;
+            buf = xrealloc(buf, buf_cap);
+        }
+
+        *file_sz += fread(&buf[*file_sz], 1, buf_cap - *file_sz, f);
+        PERROR_IF(ferror(f), "fread");
+    }
+
+    PERROR_IF(fclose(f) != 0, "fclose");
+    return buf;
+}
+
+int main(int argc, char *argv[])
+{
+    long int size = findSize(argv[1]);
+    printf("%ld", size);
     return 0;
 }
