8a6a5a500790bd3f0209aef58787c95e3cb88937
Reza Qorbani <reza3qorbani@gmail.com>
2021-07-15 22:20:56 +0200

    add huffman encoding

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PERROR_IF(cnd, msg) \
     do                      \
@@ -13,7 +14,294 @@
         }                   \
     } while (0)
 
-long int findSize(char *file_name)
+// This constant can be avoided by explicitly
+// calculating height of Huffman Tree
+#define MAX_TREE_HT 100
+
+// A Huffman tree node
+struct MinHeapNode
+{
+
+    // One of the input characters
+    char data;
+
+    // Frequency of the character
+    unsigned freq;
+
+    // Left and right child of this node
+    struct MinHeapNode *left, *right;
+};
+
+// A Min Heap:  Collection of
+// min-heap (or Huffman tree) nodes
+struct MinHeap
+{
+
+    // Current size of min heap
+    unsigned size;
+
+    // capacity of min heap
+    unsigned capacity;
+
+    // Array of minheap node pointers
+    struct MinHeapNode **array;
+};
+
+// A utility function allocate a new
+// min heap node with given character
+// and frequency of the character
+struct MinHeapNode *newNode(char data, unsigned freq)
+{
+    struct MinHeapNode *temp = (struct MinHeapNode *)malloc(
+        sizeof(struct MinHeapNode));
+
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+
+    return temp;
+}
+
+// A utility function to create
+// a min heap of given capacity
+struct MinHeap *createMinHeap(unsigned capacity)
+
+{
+
+    struct MinHeap *minHeap = (struct MinHeap *)malloc(sizeof(struct MinHeap));
+
+    // current size is 0
+    minHeap->size = 0;
+
+    minHeap->capacity = capacity;
+
+    minHeap->array = (struct MinHeapNode **)malloc(
+        minHeap->capacity * sizeof(struct MinHeapNode *));
+    return minHeap;
+}
+
+// A utility function to
+// swap two min heap nodes
+void swapMinHeapNode(struct MinHeapNode **a,
+                     struct MinHeapNode **b)
+
+{
+
+    struct MinHeapNode *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// The standard minHeapify function.
+void minHeapify(struct MinHeap *minHeap, int idx)
+
+{
+
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx)
+    {
+        swapMinHeapNode(&minHeap->array[smallest],
+                        &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// A utility function to check
+// if size of heap is 1 or not
+int isSizeOne(struct MinHeap *minHeap)
+{
+
+    return (minHeap->size == 1);
+}
+
+// A standard function to extract
+// minimum value node from heap
+struct MinHeapNode *extractMin(struct MinHeap *minHeap)
+
+{
+
+    struct MinHeapNode *temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+
+    return temp;
+}
+
+// A utility function to insert
+// a new node to Min Heap
+void insertMinHeap(struct MinHeap *minHeap,
+                   struct MinHeapNode *minHeapNode)
+
+{
+
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+
+    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq)
+    {
+
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+
+    minHeap->array[i] = minHeapNode;
+}
+
+// A standard function to build min heap
+void buildMinHeap(struct MinHeap *minHeap)
+
+{
+
+    int n = minHeap->size - 1;
+    int i;
+
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+// A utility function to print an array of size n
+void printArr(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d", arr[i]);
+
+    printf("\n");
+}
+
+// Utility function to check if this node is leaf
+int isLeaf(struct MinHeapNode *root)
+
+{
+
+    return !(root->left) && !(root->right);
+}
+
+// Creates a min heap of capacity
+// equal to size and inserts all character of
+// data[] in min heap. Initially size of
+// min heap is equal to capacity
+struct MinHeap *createAndBuildMinHeap(char data[],
+                                      int freq[], int size)
+
+{
+
+    struct MinHeap *minHeap = createMinHeap(size);
+
+    for (int i = 0; i < size; ++i)
+        minHeap->array[i] = newNode(data[i], freq[i]);
+
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+
+    return minHeap;
+}
+
+// The main function that builds Huffman tree
+struct MinHeapNode *buildHuffmanTree(char data[],
+                                     int freq[], int size)
+
+{
+    struct MinHeapNode *left, *right, *top;
+
+    // Step 1: Create a min heap of capacity
+    // equal to size.  Initially, there are
+    // modes equal to size.
+    struct MinHeap *minHeap = createAndBuildMinHeap(data, freq, size);
+
+    // Iterate while size of heap doesn't become 1
+    while (!isSizeOne(minHeap))
+    {
+
+        // Step 2: Extract the two minimum
+        // freq items from min heap
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+
+        // Step 3:  Create a new internal
+        // node with frequency equal to the
+        // sum of the two nodes frequencies.
+        // Make the two extracted node as
+        // left and right children of this new node.
+        // Add this node to the min heap
+        // '$' is a special value for internal nodes, not
+        // used
+        top = newNode('$', left->freq + right->freq);
+
+        top->left = left;
+        top->right = right;
+
+        insertMinHeap(minHeap, top);
+    }
+
+    // Step 4: The remaining node is the
+    // root node and the tree is complete.
+    return extractMin(minHeap);
+}
+
+// Prints huffman codes from the root of Huffman Tree.
+// It uses arr[] to store codes
+void printCodes(struct MinHeapNode *root, int arr[],
+                int top)
+
+{
+
+    // Assign 0 to left edge and recur
+    if (root->left)
+    {
+
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+
+    // Assign 1 to right edge and recur
+    if (root->right)
+    {
+
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+
+    // If this is a leaf node, then
+    // it contains one of the input
+    // characters, print the character
+    // and its code from arr[]
+    if (isLeaf(root))
+    {
+
+        printf("%c: ", root->data);
+        printArr(arr, top);
+    }
+}
+
+// The main function that builds a
+// Huffman Tree and print codes by traversing
+// the built Huffman Tree
+void HuffmanCodes(char data[], int freq[], int size)
+
+{
+    // Construct Huffman Tree
+    struct MinHeapNode *root = buildHuffmanTree(data, freq, size);
+
+    // Print Huffman codes using
+    // the Huffman tree built above
+    int arr[MAX_TREE_HT], top = 0;
+
+    printCodes(root, arr, top);
+}
+
+size_t findSize(char *file_name)
 {
     // opening the file in read mode
     FILE *fp = fopen(file_name, "r");
@@ -28,7 +316,7 @@ long int findSize(char *file_name)
     fseek(fp, 0L, SEEK_END);
 
     // calculating the size of the file
-    long int res = ftell(fp);
+    size_t res = ftell(fp);
 
     // closing the file
     fclose(fp);
@@ -50,13 +338,13 @@ static void *xrealloc(void *ptr, size_t size)
     return ptr;
 }
 
-static uint8_t *read_file(const char *filename, size_t *file_sz)
+static char *read_file(const char *filename, size_t *file_sz)
 {
     FILE *f;
-    uint8_t *buf;
+    char *buf;
     size_t buf_cap;
 
-    f = fopen(filename, "rb");
+    f = fopen(filename, "r");
     PERROR_IF(f == NULL, "fopen");
 
     buf_cap = 4096;
@@ -71,7 +359,7 @@ static uint8_t *read_file(const char *filename, size_t *file_sz)
             buf = xrealloc(buf, buf_cap);
         }
 
-        *file_sz += fread(&buf[*file_sz], 1, buf_cap - *file_sz, f);
+        *file_sz += fread(&buf[*file_sz], sizeof(char), buf_cap - *file_sz, f);
         PERROR_IF(ferror(f), "fread");
     }
 
@@ -79,9 +367,63 @@ static uint8_t *read_file(const char *filename, size_t *file_sz)
     return buf;
 }
 
+static void write_file(const char *filename, const uint8_t *data, size_t n)
+{
+    FILE *f;
+
+    f = fopen(filename, "w");
+    PERROR_IF(f == NULL, "fopen");
+    PERROR_IF(fwrite(data, 1, n, f) != n, "fwrite");
+    PERROR_IF(fclose(f) != 0, "fclose");
+}
+
+int calculate_frequencies(char *data, size_t data_size, uint8_t *absolut_frequencies)
+{
+    int number_of_unique_charaters = 0;
+
+    for (int i = 0; i < data_size; i++)
+    {
+        uint8_t ascii_value = (uint8_t)data[i];
+        if (absolut_frequencies[ascii_value] == 0)
+            number_of_unique_charaters++;
+
+        absolut_frequencies[ascii_value]++;
+    }
+    return number_of_unique_charaters;
+}
+
 int main(int argc, char *argv[])
 {
-    long int size = findSize(argv[1]);
-    printf("%ld", size);
+    if (argc != 2)
+    {
+        printf("usage: zipnow filename\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *file_name = argv[1];
+    size_t file_size = 0;
+    char *buffer = read_file(file_name, &file_size);
+
+    uint8_t absolut_frequencies[256];
+    memset(absolut_frequencies, 0, 256);
+
+    int unique_characters = calculate_frequencies(buffer, file_size, absolut_frequencies);
+
+    char *characters = (char *)malloc(unique_characters);
+    int *freqs = (int *)malloc(unique_characters);
+
+    int j = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        if (absolut_frequencies[i] != 0)
+        {
+            characters[j] = (char)i;
+            freqs[j] = absolut_frequencies[i];
+            j++;
+        }
+    }
+    int character_arr_size = sizeof(characters) / sizeof(characters[0]);
+    HuffmanCodes(characters, freqs, character_arr_size);
+
     return 0;
 }
