e7ddafccbadc43c0c09261a099dbad82f663eff3
Reza Qorbani <reza3qorbani@gmail.com>
2021-07-27 21:47:38 +0200

    write huffman codes to file

--- main.c
+++ main.c
@@ -181,7 +181,8 @@ void insert_element(int arr[], int n, char **char_element)
         strcpy(*char_element + i, digit);
     }
 
-    if(n>0) *(*char_element + n) = '\0';
+    if (n > 0)
+        *(*char_element + n) = '\0';
 }
 
 // Utility function to check if this node is leaf
@@ -342,11 +343,11 @@ static void *xrealloc(void *ptr, size_t size)
     return ptr;
 }
 
-static char *read_file(const char *filename, size_t *file_sz)
+static char *read_file(const char *filename, long int *file_sz)
 {
     FILE *f;
     char *buf;
-    size_t buf_cap;
+    long int buf_cap;
 
     f = fopen(filename, "r");
     PERROR_IF(f == NULL, "fopen");
@@ -399,19 +400,54 @@ int calculate_frequencies(char *data, long int data_size, int *absolut_frequenci
     return number_of_unique_charaters;
 }
 
-bool write_frequency(char* file_name, char * characters, int* frequncies, size_t number_of_characters)
+bool write_frequency(char *file_name, char *characters, int *frequncies, size_t number_of_characters)
 {
-    FILE *file_pointer = fopen(file_name, "w"); 
-    for(int i = 0; i < number_of_characters; i++)
+    FILE *file_pointer = fopen(file_name, "w");
+    fprintf(file_pointer,"%d§", number_of_characters); 
+    for (int i = 0; i < number_of_characters; i++)
     {
-        fprintf("%c%d", characters + i, frequncies + i); 
+        fprintf(file_pointer, "%c%d", characters[i], frequncies[i]);
     }
+    fprintf(file_pointer, "§§");
+    fclose(file_pointer); 
+}
+
+bool write_body(char *file_name_dst, char* data, uint32_t data_size,  char *huffman_code_map[256]){
+    FILE *fp = fopen(file_name_dst, "a"); 
+    for(uint32_t i = 0; i < data_size; i++){
+        int character = (int) data[i]; 
+        fprintf(fp, "%s", huffman_code_map[character]); 
+    }
+    fclose(fp); 
+    return true; 
+}
+
+char *get_file_name_no_extension(char *file_name)
+{
+
+    size_t file_name_length = 0;
+
+    for (; file_name_length <= strlen(file_name); file_name_length++)
+    {
+        if (file_name[file_name_length] == '.')
+        {
+            break;
+        }
+    }
+    char *no_extension = malloc(file_name_length*sizeof(char)+1);
+    strncpy(no_extension, file_name, file_name_length);
+    strcat(no_extension, "\0"); 
+
+    return no_extension;
 }
 
 bool write_zip(char *file_name)
 {
+
     long int file_size = 0;
     char *buffer = read_file(file_name, &file_size);
+     
+    file_size = 13;
 
     int absolut_frequencies[256];
     memset(absolut_frequencies, 0L, 256 * sizeof(int));
@@ -441,19 +477,21 @@ bool write_zip(char *file_name)
     }
 
     HuffmanCodes(characters, freqs, unique_characters, huffman_codes);
-    int k = 1; 
-    for (int i = 0; i < 256; i++)
-    {
-        if (strcmp(huffman_codes[i], "$") != 0)
-        {
 
-        }
-    }
+    char * without_extension = get_file_name_no_extension(file_name);
+    strcat(without_extension, ".zip");
 
+    write_frequency(without_extension, characters, freqs, unique_characters);
+
+    write_body(without_extension, buffer, file_size, huffman_codes); 
+    
     free(characters);
     free(freqs);
+    //free(without_extension);
     return true;
 }
+
+
 
 int main(int argc, char *argv[])
 {
@@ -463,7 +501,10 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     char *file_name = argv[1];
+    
+    
     write_zip(file_name);
 
+    
     return 0;
 }
